224248bb40fe1f5e9fdf10bce1966051743e9cc1
luke2327 <qwepoi8526@gmail.com>
2021-07-04 03:15:37 +0900

    Alg378

---
+++ 300-400/Alg378_ex01.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+    int a[5][5] = { 0 }, b[5][5] = { 0 };
+
+    k = 0;
+    for (i = 4; i >= 0; i--) {
+        for (j = i; j <= 4; j++) {
+            k++;
+            a[i][j] = k;
+        }
+    }
+
+    for (i = 0; i <= 4; i++) {
+        for (j = 0; j <= 4; j++) {
+            b[j][4 - i] = a[i][j];
+        }
+    }
+
+    for (i = 0; i <=4; i++) {
+        for (j = 0; j <=4; j++) {
+            printf("%3d", a[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (i = 0; i <=4; i++) {
+        for (j = 0; j <=4; j++) {
+            printf("%3d", b[i][j]);
+        }
+
+        printf("\n");
+    }
+}
