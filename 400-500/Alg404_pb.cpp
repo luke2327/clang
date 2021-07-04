0bba0b300ceff55bb2334923d2fca137ba3c7a0e
luke2327 <qwepoi8526@gmail.com>
2021-07-05 02:39:06 +0900

    Alg404

---
+++ 400-500/Alg404_pb.cpp
@@ -0,0 +1,38 @@
+// algorithm 404p predict bank - 7
+//
+// 2021-07-05
+
+#include <stdio.h>
+
+int main () {
+    int a[9][9] = { 0 };
+    int q = 0,
+        e = 0,
+        c = 0,
+        t = 0,
+        j = 0;
+    
+    c = (int)(9 / 2 + 1);
+
+    for (t = 0; t < 9; t++) {
+        if (t < c) {
+            e = 9 - t;
+        } else {
+            e = t + 1;
+        }
+
+        for (j = 0; j < e; j++) {
+            q++;
+
+            a[j][t] = q;
+        }
+    }
+
+    for (int i = 0; i < 9; i++) {
+        for (int o = 0; o < 9; o++) {
+            printf("%3d", a[i][o]);
+        }
+
+        printf("\n");
+    }
+}
